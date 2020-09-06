d90192db142cfe0b5a75d78a79a69c83a3645757
Wandering Robot <ej_braun@yahoo.com>
2020-09-06 12:34:15 -0700

    ok NOW it's all pythonic, missed the main module

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+"""main entry to code"""
+
 import pygame as py
 
 from regulatory.gui import GUI
@@ -15,31 +17,24 @@ class Main:
 
     def call_gui(self):
         """uses GUI to obtain user input, returns parameters in dict form"""
-        pass
 
     def unpack_gui(self):
         """goes through the info gathered from GUI and distributes it approproately"""
-        pass
 
     def get_data(self):
         """uses Keeper to obtain data"""
-        pass
 
     def create_memory(self):
         """uses knowledge data to create AI's memory"""
-        pass
 
     def create_environment(self):
         """uses environment data to create AI's environment"""
-        pass
 
     def handle_input(self):
         """uses Handler to interpret user input"""
-        pass
 
     def run_program(self):
         """main loop for learning/displaying"""
-        pass
 
 if __name__ == "__main__":
     Main()
